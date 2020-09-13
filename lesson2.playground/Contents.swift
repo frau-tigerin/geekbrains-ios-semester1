20e5fb2517b75260c02a2bb07e6f165d4fc64988
Valerie <valerie.solovyova@gmail.com>
2020-09-13 23:31:31 +0300

    lesson 2, tasks 1-5

---
+++ lesson2.playground/Contents.swift
@@ -0,0 +1,62 @@
+import UIKit
+
+// MARK: -
+// 1. Написать функцию, которая определяет, четное число или нет.
+func isNumberEven(_ n: Int) -> Bool {
+    let evenNumber = n % 2 == 0
+    return evenNumber
+}
+
+// MARK: -
+// 2. Написать функцию, которая определяет, делится ли число без остатка на 3.
+func isNumberDevidedByThree(_ n: Int) -> Bool {
+    let numberDevidedByThree = n % 3 == 0
+    return numberDevidedByThree
+}
+
+// MARK: -
+// 3. Создать возрастающий массив из 100 чисел.
+func ascendingArray(length: UInt, startingFrom startValue: Int) -> [Int] {
+    var numbers = [Int]()
+    var value = startValue
+    while numbers.count < length {
+        numbers.append(value)
+        value += 1
+    }
+    return numbers
+}
+
+func ascendingArray2(length: UInt, startingFrom startValue: Int) -> [Int] {
+    var numbers = [Int]()
+    let endValue = startValue + Int(length)
+    for number in startValue..<endValue {
+        numbers.append(number)
+    }
+    return numbers
+}
+
+ascendingArray(length: 6, startingFrom: 10)
+ascendingArray2(length: 6, startingFrom: 10)
+
+// MARK: -
+// 4. Удалить из этого массива все четные числа и все числа, которые не делятся на 3.
+var array = ascendingArray(length: 100, startingFrom: 0)
+let arrayLength = array.count
+for i in 0..<arrayLength {
+    let invertedIndex = arrayLength - 1 - i
+    if isNumberEven(array[invertedIndex]) {
+        array.remove(at: invertedIndex)
+    } else if isNumberDevidedByThree(array[invertedIndex]) {
+        array.remove(at: invertedIndex)
+    }
+}
+print(array)
+
+// MARK: -
+// 5. * Написать функцию, которая добавляет в массив новое число Фибоначчи, и добавить при помощи нее 100 элементов.
+func appendFibonacciNumber(array: [Int]) -> [Int] {
+    var fibonacciArray = array
+    let fibonacciNumber = array[array.count - 1] + array[array.count - 2]
+    fibonacciArray.append(fibonacciNumber)
+    return fibonacciArray
+}
