280acf2dd54b2e5b5c9531f696ef99139847f988
Valerie <valerie.solovyova@gmail.com>
2020-09-27 00:13:20 +0300

    lesson5

--- lesson5.playground/Contents.swift
+++ lesson5.playground/Contents.swift
@@ -1,9 +1,8 @@
 import Foundation
 
 protocol Car {
-    var modelYear: Int { get set }
-    var fuel: UInt { get set }
     var carBrand: Brand { get set }
+    var modelYear: Int { get set }
     var engineState: EngineState { get set }
     var windowsState: WindowState { get set }
     func apply(action: Action)
@@ -22,52 +21,45 @@ enum WindowState {
 }
 
 enum Action {
-    case engineOn
-    case engineOff
-    case closeWindow
-    case openWindow
     case loadCargoToBody(cargoCapacity: Double)
     case unloadCargoFromBody(cargoCapacity: Double)
     case loadGoodsToTrunk(goodsVolume: Double)
     case unloadGoodsFromTrunk(goodsVolume: Double)
-    case spendFuel(capacity: UInt)
 }
 
 extension Car {
-    mutating func apply(action: Action) {
-        
-        switch action {
-        case .engineOn:
-            engineState = .on
-        case .engineOff:
+    mutating func handleEngine() {
+        switch engineState {
+        case .on:
             engineState = .off
-        case .closeWindow:
+        case .off:
+            engineState = .on
+        }
+    }
+    
+    mutating func handleWindows() {
+        switch windowsState {
+        case .open:
             windowsState = .closed
-        case .openWindow:
+        case .closed:
             windowsState = .open
-        case .spendFuel(let capacity):
-            fuel -= capacity
-        default:
-            break
         }
     }
 }
 
 class SportCar: Car, CustomStringConvertible {
-    var modelYear: Int
-    var fuel: UInt
     var carBrand: Brand
+    var modelYear: Int
     var engineState: EngineState
     var windowsState: WindowState
     let trunkCapacity: Double
     var filledTrunkCapacity: Double
     
-    init(carBrand: Brand, modelYear: Int, engineState: EngineState, windowsState: WindowState, fuel: UInt, trunkCapacity: Double, filledTrunkCapacity: Double) {
+    init(carBrand: Brand, modelYear: Int, engineState: EngineState, windowsState: WindowState, trunkCapacity: Double, filledTrunkCapacity: Double) {
         self.carBrand = carBrand
         self.modelYear = modelYear
         self.engineState = engineState
         self.windowsState = windowsState
-        self.fuel = fuel
         self.trunkCapacity = trunkCapacity
         self.filledTrunkCapacity = filledTrunkCapacity
     }
@@ -76,38 +68,31 @@ class SportCar: Car, CustomStringConvertible {
         switch action {
         case .loadGoodsToTrunk(let capacity):
             filledTrunkCapacity += capacity
-        
         case .unloadGoodsFromTrunk(let capacity):
             filledTrunkCapacity -= capacity
-        
-        case .spendFuel(let capacity):
-            fuel -= capacity
-        
         default:
             apply(action: action)
         }
     }
     
     var description: String {
-        "\(carBrand), \(modelYear), \(engineState), \(windowsState), \(trunkCapacity), \(filledTrunkCapacity), \(fuel)"
+        "\(carBrand), \(modelYear), \(engineState), \(windowsState), \(trunkCapacity), \(filledTrunkCapacity)"
     }
 }
 
 class TruckCar: Car, CustomStringConvertible {
-    var modelYear: Int
-    var fuel: UInt
     var carBrand: Brand
+    var modelYear: Int
     var engineState: EngineState
     var windowsState: WindowState
     let bodyCapacity: Double
     var filledBodyCapacity: Double
     
-    init(carBrand: Brand, modelYear: Int, engineState: EngineState, windowsState: WindowState, fuel: UInt, bodyCapacity: Double, filledBodyCapacity: Double) {
+    init(carBrand: Brand, modelYear: Int, engineState: EngineState, windowsState: WindowState, bodyCapacity: Double, filledBodyCapacity: Double) {
         self.carBrand = carBrand
         self.modelYear = modelYear
         self.engineState = engineState
         self.windowsState = windowsState
-        self.fuel = fuel
         self.bodyCapacity = bodyCapacity
         self.filledBodyCapacity = filledBodyCapacity
     }
@@ -124,21 +109,21 @@ class TruckCar: Car, CustomStringConvertible {
     }
     
     var description: String {
-        "\(carBrand), \(modelYear), \(engineState), \(windowsState), \(bodyCapacity), \(filledBodyCapacity), \(fuel)"
+        "\(carBrand), \(modelYear), \(engineState), \(windowsState), \(bodyCapacity), \(filledBodyCapacity)"
     }
 }
 
 
-var car1 = SportCar(carBrand: .mercedes, modelYear: 2004, engineState: .off, windowsState: .closed, fuel: 150, trunkCapacity: 200, filledTrunkCapacity: 150)
+var car1 = SportCar(carBrand: .mercedes, modelYear: 2004, engineState: .off, windowsState: .closed, trunkCapacity: 200, filledTrunkCapacity: 150)
 print(car1)
 car1.apply(action: .loadGoodsToTrunk(goodsVolume: 10))
 print(car1)
 
-car1.apply(action: .spendFuel(capacity: 100))
-print(car1)
-
-var car2 = TruckCar(carBrand: .volvo, modelYear: 1998, engineState: .on, windowsState: .open, fuel: 300, bodyCapacity: 500, filledBodyCapacity: 400)
+var car2 = TruckCar(carBrand: .volvo, modelYear: 1998, engineState: .on, windowsState: .open, bodyCapacity: 500, filledBodyCapacity: 400)
 print(car2)
 
-car2.apply(action: .closeWindow)
+car2.handleEngine()
+print(car2)
 
+car2.handleWindows()
+print(car2)
